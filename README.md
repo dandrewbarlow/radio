2bd59ec57e93bfedd8fe186494b29eaafba6a0f4
Andrew Barlow <dandrewbarlow@gmail.com>
2023-12-17 02:13:00 -0600

    init

---
+++ README.md
@@ -0,0 +1,94 @@
+# radio.py
+
+Written by Andrew Barlow ([github](https://github.com/dandrewbarlow) | [website](https://a-barlow.com))
+
+## Description
+
+A simple CLI internet radio interface for old internet fans. Requires `fzf`,
+`vlc`, and python libraries `fzflib` & `rich`.
+
+### Goals
+
+This project was written because of my fondness for old internet ideas that
+have gone out of fashion. Internet radio is one of those cool-in-theory ideas
+that has a bunch of hurdles in the way of the actual experience. GUIs are old
+and outdated, station lists are pulled from weird sources, editing your saved
+stations can result in menu hell, internet services require accounts and try to
+upcharge you every step of the way. I wanted a radio player within the
+terminal, I wanted to choose and organize my radio stations myself in a
+plaintext file that I could work my vim-fu on, and I didn't want to see ads or
+get emails about my radio player. There are more feature-rich clients
+available, but if you just want to play some dang music while you're coding
+give this script a shot.
+
+## Install Dependencies
+
+### Libraries
+
+For Python libraries, enter console at project folder and run: 
+
+`python3 -m pip install -r requirements.txt`
+
+### Applications
+
+Install [fzf](https://github.com/junegunn/fzf) and
+[VLC](https://www.videolan.org/vlc/) for your platform of choice. Very handy if
+you don't use already.
+
+## Usage
+
+Place all internet radio stream urls in a file called `config.txt` within the
+project directory. This could alternately be changed in the python file (see
+the variable `fp`). To parse the station metadata, please format as following:
+
+```
+# Station Name
+https://station-url.com
+```
+
+Extraneous comments will not be parsed, but any lines starting with `http` will
+search the line above it for a commented name. If none is found, it will
+default to the stream url.
+
+Run using Python:
+
+```
+python3 radio.py
+```
+
+For \*NIX users like myself the shebang should default to the user's default
+python3 binary so it be capable of running with a simple `./radio.py`.
+
+### Station Recs
+
+You gotta find the stream urls for yourself. This is the blessing/curse of this
+design, but there are many great resources out there.
+
+For those that need somewhere to start:
+
+* Collections
+    * [Soma FM](https://somafm.com/): ad free, and diverse selection of stations
+    * [RadioBrowser](https://www.radio-browser.info/): FOSS database of stations.
+      I'm iffy on their quality, but I've heard enough people recommend that it
+      there's gotta be a reason.
+    * [BBC
+      Stations](https://en.everybodywiki.com/List_of_BBC_radio_stream_URLs):
+      something for everybody
+* Individual Stations (matters of taste, obvs)
+    * [KEXP](https://www.kexp.org/): the GOAT. Their [streaming urls are hidden
+      without some googling, though](https://www.kexp.org/streaming-urls/)
+    * [Triple J](https://www.abc.net.au/triplej): another GOAT. See [direct
+      streams
+      here](https://help.abc.net.au/hc/en-us/articles/4402927208079-Where-can-I-find-direct-stream-URLs-for-ABC-Radio-stations-)
+      or [time-delayed streams
+      here](https://www.abc.net.au/triplej/time-delayed-streams/9445252)
+
+## Limitations
+
+For minimalism's sake, I am hiding the VLC `stderr` output. This also means if
+something goes wrong, it will not tell you. A safe troubleshooting guide is to
+double check that the url is correct, and can be listened to with VLC's GUI.
+This is not the most complex script, and is designed for terminal users, so I'm
+not imagining that much can go wrong, and if it does, I'm betting the user can
+figure it out. Feel free to leave an issue if you like this but something's not
+working for you. Trying to keep this simple though.
---
+++ radio.py
@@ -0,0 +1,165 @@
+#! /usr/bin/env python3
+# Andrew Barlow
+# radio.py
+# 
+# Description
+# simple CLI internet radio interface
+
+# IMPORTS ##################################################
+
+# python libs
+import subprocess
+import os
+
+# rich: pretty python scripts
+# https://rich.readthedocs.io/en/stable/index.html
+from rich import print
+from rich.console import Console
+from rich.table import Table
+from rich.traceback import install
+
+# im lazy
+import fzflib
+
+# INIT ##################################################
+
+# location of radio stations + metadata
+# see README.md for formatting instructions
+config_file = 'config.txt'
+
+script_dir = os.path.abspath( os.path.dirname( __file__ ) )
+
+# init rich traceback
+install(show_locals=True)
+
+# init rich console
+console = Console();
+
+# init fuzzy finder
+fzf = fzflib.FZF()
+
+# clear screen b4 running
+console.clear()
+
+# HELPER ##################################################
+
+# read file into an array of its lines
+def readFile(filepath):
+    # define list file
+    f = os.path.join(script_dir, filepath)
+
+    # open that file
+    file = open(f, 'r')
+
+    # read file into an array
+    lines = file.readlines()
+
+    return lines;
+
+def parse_stations(filepath):
+
+    lines = readFile(filepath)
+
+    # prep to loop through lines
+    lineNum = 0
+    stations = []
+
+    # loop through lines
+    for line in lines:
+
+        lineNum += 1
+
+        # if a url is found
+        if line[0:4] == 'http':
+
+            url = line.rstrip()
+
+            station_name = ""
+
+            # if no station name found, use url as ref
+            if lines[lineNum-2][0] == '#':
+                station_name = lines[lineNum-2][2:].strip()
+            else:
+                station_name = url
+
+            # parse station data into dict
+            station = {
+                    'name': station_name,
+                    'url': url
+            }
+
+            # add to station list
+            stations.append(station);
+
+    return stations
+
+# UI to pick which station to listen to
+def pick_station(stations):
+    # create a list of just the names for fzf browsing
+    stationList = [ s['name'] for s in stations ]
+
+    stationList.append('Exit')
+
+    # use fzf to pick station
+    fzf.input = stationList
+
+    station_choice = fzf.prompt()
+
+    station = "Exit"
+
+    for s in stations:
+        if s['name'] == station_choice:
+            station = s
+
+    return station
+
+def play_station(station):
+
+    if (station == "Exit"):
+        return 0
+
+    display_info(station)
+
+    # KeyboardInterrupt is our exit routine, not an error
+    try:
+        subprocess.run(['cvlc', station["url"] ],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.STDOUT)
+    # TODO changing radio stations while running
+    except KeyboardInterrupt as e:
+        console.clear()
+        return 1
+
+# feedback
+def display_info(station):
+    # use rich tables for pretty output
+    # https://rich.readthedocs.io/en/stable/tables.html
+    table = Table(title="Radio", show_lines=True)
+
+    table.add_column("Station Info")
+    table.add_column("User Interface")
+    table.add_row( f'Station Name: [purple]{station["name"]}[/purple]',  'Return to menu: [red]<ctrl> + c[/red]')
+    table.add_row( f'Station URL: [blue]{station["url"]}[/blue]', 'Quit: 2x [red]<ctrl> + c[/red]')
+    
+    console.print(table)
+
+def radio(filepath):
+    stations = parse_stations(filepath)
+    station = None
+
+    try:
+        station = pick_station(stations)
+    except KeyboardInterrupt as e:
+        return 0
+
+    return play_station(station)
+
+
+# MAIN ##################################################
+
+def main():
+    # it's not hacky if it works
+    while(radio(config_file) != 0):
+        pass
+
+main()
